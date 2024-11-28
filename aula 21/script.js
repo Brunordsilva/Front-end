685dbe05a8db0999964f9b57c30715c078441e2b
Brunordsilva <br0571997@gmail.com>
2024-11-28 18:21:31 -0300

    Update script.js

--- aula 21/script.js
+++ aula 21/script.js
@@ -3,36 +3,29 @@ document.getElementById('searchButton').addEventListener('click', () => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         .then(response => response.json())
         .then(data => {
-            // Manipulando o DOM para exibir as informaçõesemonTitle
             document.getElementById('pokemonTitle').textContent = data.name.toUpperCase();
             document.getElementById('pokemonImage').src = data.sprites.front_default;
             document.getElementById('pokemonType').textContent = `Tipo(s): ${data.types.map(type => type.type.name).join(', ')}`;
-
-            // Exibindo as estatísticas
             let stats = data.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(', ');
             document.getElementById('pokemonStats').textContent = `Estatísticas: ${stats}`;
         })
         .catch(error => {
             alert('Pokémon não encontrado!');
             console.error('Erro:', error);
-
         });
-    fetch('https://pokeapi.co/api/v2/pokemon/{id}')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => console.error('Erro:', error));
 });
 
-
 document.getElementById('busque').addEventListener('click', () => {
-    let TypeId = document.getElementById('tipo').value.toLowerCase();
-    fetch(`https://pokeapi.co/api/v2/type/${TypeId}}`)
+    let typeName = document.getElementById('tipo').value.toLowerCase();
+    fetch(`https://pokeapi.co/api/v2/type/${typeName}`)
         .then(response => response.json())
         .then(data => {
-            document.getElementById('tiponome').textContent = `Tipo(s): ${data.types.map(type => type.type.name).join(', ')}`;
-
+            document.getElementById('tiponome').textContent = `Tipo: ${data.name.toUpperCase()}`;
+            document.getElementById('info').textContent = `Dano Duplo Contra: ${data.damage_relations.double_damage_to.map(type => type.name).join(', ')}`;
+            document.getElementById('weakness').textContent = `Apanha de 2x: ${data.damage_relations.double_damage_from.map(type => type.name).join(', ')}`;
         })
-        .catch(error => console.error('Erro:', error));
+        .catch(error => {
+            alert('Tipo não encontrado!');
+            console.error('Erro:', error);
+        });
 });
